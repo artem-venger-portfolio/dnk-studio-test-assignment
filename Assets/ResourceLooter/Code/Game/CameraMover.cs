ffe53dba3a9726310a11d12fd64327daab74097d
Venger Artem <artem.venger.professional@gmail.com>
2025-03-31 20:21:22 +0700

    Add camera move coroutine

--- Assets/ResourceLooter/Code/Game/CameraMover.cs
+++ Assets/ResourceLooter/Code/Game/CameraMover.cs
@@ -9,6 +9,8 @@ namespace ResourceLooter
         private readonly ICoroutineController _coroutineController;
         private readonly Camera _camera;
         private Coroutine _moveCoroutine;
+        private Vector3 _targetPosition;
+        private Vector3 _velocity;
         private bool _isDragging;
 
         public CameraMover(ClickAndDragDetector clickAndDragDetector, Camera camera,
@@ -33,6 +35,12 @@ namespace ResourceLooter
             _clickAndDragDetector.DragFinished -= DragFinishedEventHandler;
         }
 
+        private Vector3 CameraPosition
+        {
+            get => _camera.transform.position;
+            set => _camera.transform.position = value;
+        }
+
         private void DragStartedEventHandler(Vector2 screenPosition)
         {
             _isDragging = true;
@@ -50,6 +58,7 @@ namespace ResourceLooter
 
         private void DraggingEventHandler(Vector2 screenPosition)
         {
+            _targetPosition = _camera.ScreenToWorldPoint(screenPosition);
         }
 
         private void DragFinishedEventHandler(Vector2 screenPosition)
@@ -59,7 +68,37 @@ namespace ResourceLooter
 
         private IEnumerator GetMoveCoroutine()
         {
-            yield break;
+            _targetPosition = CameraPosition;
+            _velocity = Vector3.zero;
+
+            while (_isDragging)
+            {
+                var distanceToTarget = Vector3.Distance(CameraPosition, _targetPosition);
+                const float acceptable_distance = 0.0001f;
+                var isOnTarget = distanceToTarget < acceptable_distance;
+                if (isOnTarget == false)
+                {
+                    const float follow_time = 0.5f;
+                    CameraPosition = Vector3.SmoothDamp(CameraPosition, _targetPosition, ref _velocity, follow_time);
+                }
+
+                yield return null;
+            }
+
+            var acceleration = Vector3.zero;
+            const float max_acceleration = float.PositiveInfinity;
+            const float min_deceleration_velocity = 0.5f;
+            while (_velocity.magnitude > min_deceleration_velocity)
+            {
+                var deltaTime = Time.deltaTime;
+                CameraPosition += _velocity * deltaTime;
+                const float deceleration_time = 1f;
+                _velocity = Vector3.SmoothDamp(_velocity, Vector3.zero, ref acceleration, deceleration_time,
+                                               max_acceleration, deltaTime);
+                yield return null;
+            }
+
+            _moveCoroutine = null;
         }
     }
 }
