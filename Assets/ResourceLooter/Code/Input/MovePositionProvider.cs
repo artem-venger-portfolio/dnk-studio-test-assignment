04d79e5b9f6368096eec901ea31f38751e69f593
Venger Artem <artem.venger.professional@gmail.com>
2025-03-31 13:23:13 +0700

    Create basic MovePositionProvider

---
+++ Assets/ResourceLooter/Code/Input/MovePositionProvider.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+namespace ResourceLooter
+{
+    public class MovePositionProvider
+    {
+        private readonly InputReceiver _inputReceiver;
+
+        public MovePositionProvider(InputReceiver inputReceiver)
+        {
+            _inputReceiver = inputReceiver;
+        }
+
+        public event Action<Vector3> PositionChanged;
+
+        public void Enable()
+        {
+            _inputReceiver.MovePressed += MovePressedEventHandler;
+        }
+
+        public void Disable()
+        {
+            _inputReceiver.MovePressed -= MovePressedEventHandler;
+        }
+
+        private void MovePressedEventHandler()
+        {
+            Debug.Log(nameof(MovePressedEventHandler));
+            PositionChanged?.Invoke(Vector3.zero);
+        }
+    }
+}
