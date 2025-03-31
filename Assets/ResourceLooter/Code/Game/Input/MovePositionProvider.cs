e32d69c3b346f29ffd95b58f1f4832a2c667ef5d
Venger Artem <artem.venger.professional@gmail.com>
2025-03-31 14:58:12 +0700

    Calculated world position on plane

--- Assets/ResourceLooter/Code/Game/Input/MovePositionProvider.cs
+++ Assets/ResourceLooter/Code/Game/Input/MovePositionProvider.cs
@@ -7,12 +7,14 @@ namespace ResourceLooter
     {
         private readonly InputReceiver _inputReceiver;
         private readonly Camera _camera;
+        private readonly Plane _plane;
         private Vector2 _lastScreenPosition;
 
         public MovePositionProvider(InputReceiver inputReceiver, Camera camera)
         {
             _inputReceiver = inputReceiver;
             _camera = camera;
+            _plane = new Plane(Vector3.up, Vector3.zero);
         }
 
         public event Action<Vector3> PositionChanged;
@@ -31,8 +33,20 @@ namespace ResourceLooter
 
         private void MovePressedEventHandler()
         {
-            var lastWorldPosition = _camera.ScreenToWorldPoint(_lastScreenPosition);
+            var lastWorldPosition = GetLastWorldPosition();
             PositionChanged?.Invoke(lastWorldPosition);
+        }
+
+        private Vector3 GetLastWorldPosition()
+        {
+            var ray = _camera.ScreenPointToRay(_lastScreenPosition);
+            if (_plane.Raycast(ray, out var distance))
+            {
+                return ray.GetPoint(distance);
+            }
+
+            throw new Exception("Can't get the corresponding world position for the " +
+                                $"last screen position {_lastScreenPosition}");
         }
 
         private void PointerPositionChanged(Vector2 screenPosition)
