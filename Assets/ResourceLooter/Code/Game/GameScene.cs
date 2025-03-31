36b9b1b4ba6997ddc54f6a2814ae4812dfaa3574
Venger Artem <artem.venger.professional@gmail.com>
2025-03-31 13:27:10 +0700

    Create instance of MovePositionProvider

--- Assets/ResourceLooter/Code/Game/GameScene.cs
+++ Assets/ResourceLooter/Code/Game/GameScene.cs
@@ -6,5 +6,18 @@ namespace ResourceLooter
     {
         [SerializeField]
         private InputReceiver _inputReceiver;
+
+        private MovePositionProvider _movePositionProvider;
+
+        private void Start()
+        {
+            StartGame();
+        }
+
+        public void StartGame()
+        {
+            _movePositionProvider = new MovePositionProvider(_inputReceiver);
+            _movePositionProvider.Enable();
+        }
     }
 }
