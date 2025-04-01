033aef312e14e91399b6d8fe48dfd97a32fc1321
Venger Artem <artem.venger.professional@gmail.com>
2025-04-01 19:55:57 +0700

    Add logic to inventory entry

--- Assets/ResourceLooter/Code/Game/UI/Inventory/InventoryEntry.cs
+++ Assets/ResourceLooter/Code/Game/UI/Inventory/InventoryEntry.cs
@@ -1,8 +1,43 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 
 namespace ResourceLooter
 {
     public class InventoryEntry : MonoBehaviour
     {
+        [SerializeField]
+        private TMP_Text _nameField;
+
+        [SerializeField]
+        private TMP_Text _countField;
+
+        private ResourceType _resource;
+        private int _count;
+
+        public ResourceType Resource
+        {
+            get => _resource;
+            set
+            {
+                _resource = value;
+                _nameField.text = _resource.ToString();
+            }
+        }
+
+        public void SetCount(int value)
+        {
+            _count = value;
+            _countField.name = _count.ToString();
+        }
+
+        public void Show()
+        {
+            gameObject.SetActive(value: true);
+        }
+
+        public void Hide()
+        {
+            gameObject.SetActive(value: false);
+        }
     }
 }
