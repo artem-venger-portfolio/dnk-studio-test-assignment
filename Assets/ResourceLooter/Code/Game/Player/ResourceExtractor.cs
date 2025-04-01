69f7a672f2873dc203f5b457473607d5137976c7
Venger Artem <artem.venger.professional@gmail.com>
2025-04-01 15:48:22 +0700

    Record interacting buildings

--- Assets/ResourceLooter/Code/Game/Player/ResourceExtractor.cs
+++ Assets/ResourceLooter/Code/Game/Player/ResourceExtractor.cs
@@ -1,12 +1,16 @@
-﻿namespace ResourceLooter
+﻿using System.Collections.Generic;
+
+namespace ResourceLooter
 {
     public class ResourceExtractor
     {
         private readonly PlayerView _playerView;
+        private readonly List<BuildingBase> _interactingBuildings;
 
         public ResourceExtractor(PlayerView playerView)
         {
             _playerView = playerView;
+            _interactingBuildings = new List<BuildingBase>(capacity: 4);
         }
 
         public void Enable()
@@ -19,14 +23,17 @@
         {
             _playerView.BuildingInteractionZoneEntered -= BuildingInteractionZoneEnteredEventHandler;
             _playerView.BuildingInteractionZoneExited -= BuildingInteractionZoneExitedEventHandler;
+            _interactingBuildings.Clear();
         }
 
         private void BuildingInteractionZoneEnteredEventHandler(BuildingBase building)
         {
+            _interactingBuildings.Add(building);
         }
 
         private void BuildingInteractionZoneExitedEventHandler(BuildingBase building)
         {
+            _interactingBuildings.Remove(building);
         }
     }
 }
