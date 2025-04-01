7ca6a3a7353989d36d508447dfcfe9d3a1d6301d
Venger Artem <artem.venger.professional@gmail.com>
2025-04-01 14:53:52 +0700

    Create BuildingUI component

---
+++ Assets/ResourceLooter/Code/Game/Building/BuildingUI.cs
@@ -0,0 +1,24 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace ResourceLooter
+{
+    public class BuildingUI : MonoBehaviour
+    {
+        [SerializeField]
+        private TMP_Text _nameField;
+
+        [SerializeField]
+        private TMP_Text _countField;
+
+        public void SetName(ResourceType resource)
+        {
+            _nameField.text = resource.ToString();
+        }
+
+        public void SetCount(int count)
+        {
+            _countField.text = count.ToString();
+        }
+    }
+}
