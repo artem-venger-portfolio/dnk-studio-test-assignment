1c98dbb345a4a882d091a97865c2ffdda690e570
Venger Artem <artem.venger.professional@gmail.com>
2025-04-01 15:16:01 +0700

    Create PlayerView

---
+++ Assets/ResourceLooter/Code/Game/Player/PlayerView.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace ResourceLooter
+{
+    public class PlayerView : MonoBehaviour
+    {
+        [SerializeField]
+        private CharacterController _characterController;
+
+        [SerializeField]
+        private Animator _animator;
+
+        public CharacterController CharacterController => _characterController;
+
+        public Animator Animator => _animator;
+    }
+}
