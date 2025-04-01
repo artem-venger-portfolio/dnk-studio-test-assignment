60fde83f650a46b4b8cb284f28e131d351194c07
Venger Artem <artem.venger.professional@gmail.com>
2025-04-01 20:09:20 +0700

    Create entries and update its values

--- Assets/ResourceLooter/Code/Game/UI/Inventory/InventoryScreen.cs
+++ Assets/ResourceLooter/Code/Game/UI/Inventory/InventoryScreen.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace ResourceLooter
@@ -14,17 +15,41 @@ namespace ResourceLooter
         [SerializeField]
         private Button _closeButton;
 
+        private InventoryEntry[] _entries;
         private Inventory _inventory;
 
         public void Initialize(Inventory inventory)
         {
             _inventory = inventory;
             _closeButton.onClick.AddListener(CloseButtonClickedEventHandler);
+            CreateEntryForEachResource();
+        }
+
+        private void CreateEntryForEachResource()
+        {
+            var resources = Enum.GetValues(typeof(ResourceType));
+            _entries = new InventoryEntry[resources.Length];
+            for (var i = 0; i < resources.Length; i++)
+            {
+                var currentEntry = Instantiate(_entryTemplate, _scrollContent);
+                currentEntry.Resource = (ResourceType)resources.GetValue(i);
+                _entries[i] = currentEntry;
+            }
         }
 
         private void CloseButtonClickedEventHandler()
         {
             Close();
+        }
+
+        private void OnEnable()
+        {
+            foreach (var currentEntry in _entries)
+            {
+                var resource = currentEntry.Resource;
+                var count = _inventory.Get(resource);
+                currentEntry.SetCount(count);
+            }
         }
     }
 }
