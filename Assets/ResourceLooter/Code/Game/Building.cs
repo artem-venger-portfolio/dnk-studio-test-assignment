d7849bf20b5925583259cb6392043ff78d66c2bc
Venger Artem <artem.venger.professional@gmail.com>
2025-04-01 14:28:48 +0700

    Add production logic to a building

--- Assets/ResourceLooter/Code/Game/Building.cs
+++ Assets/ResourceLooter/Code/Game/Building.cs
@@ -6,5 +6,24 @@ namespace ResourceLooter
     {
         [SerializeField]
         private ResourceType _produce;
+
+        private int _resourcesCount;
+        private IGameConfig _config;
+        private float _elapsedTime;
+
+        public void Initialize(IGameConfig config)
+        {
+            _config = config;
+        }
+
+        private void Update()
+        {
+            _elapsedTime = Time.deltaTime;
+            if (_elapsedTime >= _config.ProductionTime)
+            {
+                _elapsedTime -= _config.ProductionTime;
+                _resourcesCount++;
+            }
+        }
     }
 }
